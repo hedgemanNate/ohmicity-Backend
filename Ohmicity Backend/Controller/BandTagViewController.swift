91800ac62ea0a9de10d1a1e8638c53cb49047495
Nate Hedgeman <47337337+hedgemanNate@users.noreply.github.com>
2022-01-06 15:28:28 -0500

    working BandTagViewController

--- Ohmicity Backend/Controller/BandTagViewController.swift
+++ Ohmicity Backend/Controller/BandTagViewController.swift
@@ -14,6 +14,7 @@ class BandTagViewController: NSViewController, NSTableViewDelegate, NSTableViewD
     //Table Views
     @IBOutlet weak var bandTableView: NSTableView!
     @IBOutlet weak var tagsTableView: NSTableView!
+    
     var bandTableIndex: Int {
         var bandTableIndex = bandTableView.selectedRow
         if bandTableIndex < 0 {
@@ -22,13 +23,17 @@ class BandTagViewController: NSViewController, NSTableViewDelegate, NSTableViewD
         return bandTableIndex
     }
     
+    var filterArray = [BandTags]()
+    
     //Text Fields
     @IBOutlet weak var newTagTextField: NSTextField!
-    @IBOutlet weak var searchTextField: NSTextField!
+    @IBOutlet weak var searchTextField: NSSearchField!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
         bandTableView.delegate = self
         bandTableView.dataSource = self
         bandTableView.doubleAction = #selector(bandTableClick)
@@ -36,26 +41,57 @@ class BandTagViewController: NSViewController, NSTableViewDelegate, NSTableViewD
         tagsTableView.delegate = self
         tagsTableView.dataSource = self
         tagsTableView.doubleAction = #selector(tagsTableClick)
-        updateViews()
+        
     }
     
-    @IBAction func searchButtonTapped(_ sender: Any) {
-    }
+    
+    //MARK: Button Functions
     
     @IBAction func deleteButtonTapped(_ sender: Any) {
+        let tag = filterArray[bandTableIndex].variations[tagsTableView.selectedRow]
+        filterArray[bandTableIndex].variations.removeAll(where: {$0 == tag})
+        tagsTableView.reloadData()
     }
 
     @IBAction func addTagButtonTapped(_ sender: Any) {
-        let tag = tagController.bandTags[bandTableIndex]
+        let tag = filterArray[bandTableIndex]
         let newTag = newTagTextField.stringValue
         tag.variations.append(newTag)
         tagsTableView.reloadData()
         localDataController.saveBandTagData()
+        newTagTextField.stringValue = ""
     }
-
+    
+    @IBAction func searchFieldSearching(_ sender: Any) {
+        filterArray = tagController.bandTags.filter({$0.variations.contains(where: {$0.localizedCaseInsensitiveContains(searchTextField.stringValue)})})
+        
+        DispatchQueue.main.async {
+            self.bandTableView.reloadData()
+            self.tagsTableView.reloadData()
+        }
+        
+        if searchTextField.stringValue == "" {
+            setFilterArray()
+        }
+    }
+    
+    
+    
+    //MARK: UpdateViews
     private func updateViews() {
         self.preferredContentSize = NSSize(width: 1320, height: 780)
-        
+        //newTagTextField.becomeFirstResponder()
+        setFilterArray()
+    }
+    
+    
+    //MARK: Functions
+    private func setFilterArray() {
+        filterArray = tagController.bandTags
+        DispatchQueue.main.async {
+            self.bandTableView.reloadData()
+            //self.tagsTableView.reloadData()
+        }
     }
     
     @objc private func bandTableClick() {
@@ -63,7 +99,7 @@ class BandTagViewController: NSViewController, NSTableViewDelegate, NSTableViewD
     }
     
     @objc private func tagsTableClick() {
-        self.newTagTextField.stringValue = "\(tagController.bandTags[self.bandTableView.selectedRow].variations[self.tagsTableView.selectedRow])"
+        self.newTagTextField.stringValue = "\(filterArray[self.bandTableView.selectedRow].variations[self.tagsTableView.selectedRow])"
     }
 }
 
@@ -71,15 +107,12 @@ class BandTagViewController: NSViewController, NSTableViewDelegate, NSTableViewD
 extension BandTagViewController {
     
     func numberOfRows(in tableView: NSTableView) -> Int {
-        let bandTag = tagController.bandTags[bandTableIndex]
-            
-        
-        
+    
         switch tableView {
         case bandTableView:
-            return tagController.bandTags.count
+            return filterArray.count
         case tagsTableView:
-            return bandTag.variations.count
+            return filterArray[bandTableIndex].variations.count
         default:
             return 0
         }
@@ -90,12 +123,12 @@ extension BandTagViewController {
         switch tableView {
         case bandTableView:
             if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier("BandTagCell"), owner: nil) as? NSTableCellView {
-                cell.textField?.stringValue = "\(row + 1): \(tagController.bandTags[row].variations[0])"
+                cell.textField?.stringValue = "\(row + 1): \(filterArray[row].variations[0])"
                 return cell
             }
             
         case tagsTableView:
-            let bandTag = tagController.bandTags[bandTableIndex]
+            let bandTag = filterArray[bandTableIndex]
             if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier("VariationCell"), owner: nil) as? NSTableCellView {
                 cell.textField?.stringValue = "\(row + 1): \(bandTag.variations[row])"
                 return cell
@@ -103,6 +136,6 @@ extension BandTagViewController {
         default:
             return NSTableCellView()
         }
-        return nil
+        return NSTableCellView()
     }
 }
