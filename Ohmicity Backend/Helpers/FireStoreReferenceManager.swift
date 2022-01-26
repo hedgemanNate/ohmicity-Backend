04fc2826b6c4b01b3e32421997f1935a533b496c
Nate Hedgeman <47337337+hedgemanNate@users.noreply.github.com>
2022-01-26 18:37:13 -0500

    added paths for production manager

--- Ohmicity Backend/Helpers/FireStoreReferenceManager.swift
+++ Ohmicity Backend/Helpers/FireStoreReferenceManager.swift
@@ -27,23 +27,27 @@ struct FireStoreReferenceManager {
 let ref = FireStoreReferenceManager.self
 
 struct WorkingOffRemoteManager {
-    static let environment = "workingData"
+    static let working = "workingData"
     static let fireDataBase = Firestore.firestore()
     
-    static let allShowDataPath = fireDataBase.collection(environment).document(environment).collection("allShowData")
-    static let allBandDataPath = fireDataBase.collection(environment).document(environment).collection("allBandData")
-    static let showDataPath = fireDataBase.collection(environment).document(environment).collection("showData")
-    static let bandDataPath = fireDataBase.collection(environment).document(environment).collection("bandData")
-    static let bandTagDataPath = fireDataBase.collection(environment).document(environment).collection("tagData")
+    static let allShowDataPath = fireDataBase.collection(working).document(working).collection("allShowData")
+    static let allBandDataPath = fireDataBase.collection(working).document(working).collection("allBandData")
+    static let showDataPath = fireDataBase.collection(working).document(working).collection("showData")
+    static let bandDataPath = fireDataBase.collection(working).document(working).collection("bandData")
+    static let bandTagDataPath = fireDataBase.collection(working).document(working).collection("tagData")
 }
 
 let workRef = WorkingOffRemoteManager.self
 
 struct ProductionManager {
-    static let environment = "productionData"
+    static let production = "productionData"
     static let fireDataBase = Firestore.firestore()
     
-    static let allShowDataPath = fireDataBase.collection(environment).document(environment).collection("allShowData")
-    static let showDataPath = fireDataBase.collection(environment).document(environment).collection("showData")
-    static let bandDataPath = fireDataBase.collection(environment).document(environment).collection("bandData")
+    static let allShowDataPath = fireDataBase.collection(production).document(production).collection("allShowData")
+    static let allVenueDataPath = fireDataBase.collection(production).document(production).collection("allVenueData")
+    static let allBandDataPath = fireDataBase.collection(production).document(production).collection("allBandData")
+    
+    static let allUserDataPath = fireDataBase.collection(production).document(production).collection("allUserData")
+    static let allBannerDataPath = fireDataBase.collection(production).document(production).collection("allBannerData")
+    
 }
